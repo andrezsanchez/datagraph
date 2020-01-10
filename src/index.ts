0b2bc3565ad2f763e0ad89f15db72992b470f139
Andre Sanchez <andrezacsanchez@gmail.com>
2020-01-10 15:40:14 -0800

    Add exports file

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { AnyProps } from './AnyProps';
+export { DataGraph } from './DataGraph';
+export { Dispatch } from './Dispatch';
+export { DataNode, ActionHandlers } from './DataNode';
+export {
+  Action,
+  makeActionCreator,
+  makePayloadlessActionCreator,
+} from './Action';
