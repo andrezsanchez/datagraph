5950866b45be3c0887aaa3341319fd47815555de
Andre Sanchez <andrezacsanchez@gmail.com>
2020-09-06 22:48:03 -0700

    Remove unused code

--- examples/hello-world/hello-world.ts
+++ examples/hello-world/hello-world.ts
@@ -1,19 +1,11 @@
 import { ConstantNode } from './../../src/ConstantNode';
 import { AnyAction } from '../../src/Action';
 import { DataGraph } from '../../src/DataGraph';
-import { DataNode } from '../../src/DataNode';
 import { ToggleAction, ToggleAction2 } from './ToggleAction';
 import { createGettableNode } from '../../src/GettableNode';
 import { createStateMachineNode } from '../../src/StateMachineNode';
 
 const graph = new DataGraph();
-
-function n(node: DataNode): DataNode {
-  if (!graph.nodeContextMap.has(node)) {
-    graph.addNode(node);
-  }
-  return node;
-}
 
 const valueNode = createStateMachineNode({
   initialValue: false,
