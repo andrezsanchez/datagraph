5058bb2867adda92446be3b9c25c7da1062e2ea4
Andre Sanchez <andrezacsanchez@gmail.com>
2020-09-07 14:23:24 -0700

    Add some exports to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ export { DataGraph } from './DataGraph';
 export { Dispatch } from './Dispatch';
 export {
   DataNode,
+  LegacyDataNode,
   ActionHandlerMap,
+  ActionHandler,
+  UnknownActionHandlerMap,
+  isDataNode,
 } from './DataNode';
 export {
   Action,
+  AnyAction,
   makeActionCreator,
 } from './Action';
+export {
+  Gettable,
+} from './Gettable';
