ad23677e568badd06ab4aee8bade84a07a7d0f41
Andre Sanchez <andrezacsanchez@gmail.com>
2020-09-07 10:06:51 -0700

    Remove unused function

--- src/Action.ts
+++ src/Action.ts
@@ -1,5 +1,4 @@
 import { FunctionalActionHandlerMap } from './StateMachineNode';
-import { ActionHandlerMap } from './DataNode';
 
 export interface Action<T extends (string | number | symbol), P>  {
   type: T;
@@ -10,6 +9,10 @@ export type AnyAction = Action<string, any>;
 
 export function makeActionCreator<T extends string>(type: T): () => Action<T, void>;
 export function makeActionCreator<T extends string, P>(type: T): (payload: P) => Action<T, P>;
+
+/**
+ * Make an action creator function. For use in action definitions.
+ */
 export function makeActionCreator<T extends string, P>(type: T) {
   return function createAction(payload: P): Action<T, P> {
     return {
@@ -17,16 +20,6 @@ export function makeActionCreator<T extends string, P>(type: T) {
       payload,
     };
   };
-}
-
-export function makeActionHandlerCreator<KV, C>(type: keyof KV) {
-  return function createActionHandler(
-    fn: (this: C, action: Action<typeof type, KV[typeof type]>) => void
-  ): ActionHandlerMap<KV> {
-    return {
-      [type]: fn,
-    } as unknown as ActionHandlerMap<KV>;
-  }
 }
 
 export function makeFnActionHandlerCreator<KV>(type: keyof KV) {
